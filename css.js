fe8155a5169905e37da4d78a8ec2e858443b336c
strakz <strakytto@gmail.com>
2016-02-04 00:35:20 +0100

    cssko

--- css.js
+++ css.js
@@ -4,5 +4,5 @@ var app = express();
 app.use(require('stylus').middleware(process.argv[3]));
 app.use(express.static(process.argv[3]));
 
-
+//tu som pridal komentar a idem to comitnut
 app.listen(process.argv[2])
